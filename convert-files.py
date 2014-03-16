cb8d87908ac52401ae3d7b31c15a63c9dca2b9f7
Brian Cho <bcho@Brians-MacBook-Pro.local>
2014-03-16 16:19:32 +0900

    Refactor monolithic convert function.

--- convert-files.py
+++ convert-files.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import logging
 import tagger
 
 def main():
@@ -9,8 +10,6 @@ def main():
 	parser.add_argument('path')
 	parser.add_argument('--from-encoding',
 		default='euc-kr')
-	parser.add_argument('--to-encoding',
-		default='utf_16')
 	parser.add_argument('--out-dir',
 		default='out/')
 	parser.add_argument('--dry-run',
@@ -27,17 +26,57 @@ def main():
 		print "Could not use out-dir %s" % args.out_dir
 		sys.exit(1)
 
-	convert(args.path, args.out_dir, args.from_encoding, args.to_encoding,
+	convert(args.path, args.out_dir, args.from_encoding,
 		args.dry_run, args.move, args.verbose)
 
 def print_verbose(verbose, line):
 	if verbose:
 		print line
 
-def convert(path, out_dir, from_encoding, to_encoding, dry_run, move, verbose):
+def is_encoding_correct(str, encoding):
+	if encoding == 'utf-16': # TODO: Is this necessary?
+		return True
+	else: 
+		try:
+			str.encode(encoding)
+			return True
+		except:
+			return False
+
+def decode_string(str, from_encoding):
+	decoded_str = str.decode(from_encoding)
+	return decoded_str
+
+def frame_strings_to_string(frame_strings):
+	str = u''
+	for framestr in frame_strings:
+		str += framestr
+	return str
+
+def create_filepath(out_dir, artist, songname):
+	if artist != None and len(artist) == 0:
+		artist = "UNKNOWN ARTIST"
+	if songname != None and len(songname) == 0:
+		songname = "UNKNOWN SONG"
+	filename = '%s - %s.mp3' % (artist, songname)
+	filename = filename.replace('\x00','').replace('/','-') # fix the problem where an errant '\x00' screws everything up
+	print filename
+	return os.path.join(out_dir, filename)
+
+def write_file(id3, filepath, move):
+	# TODO: right now, if we get the same filename it will get overwritten
+	id3.commit_to_file(filepath)
+	if move:
+		try:
+			os.remove(path)
+		except Exception, e:
+			print("Could not remove "+path)
+
+
+def convert(path, out_dir, from_encoding, dry_run, move, verbose):
 	if (os.path.isdir(path)):
 		for p in os.listdir(path):
-			convert(os.path.join(path, p), out_dir, from_encoding, to_encoding, dry_run, move, verbose)
+			convert(os.path.join(path, p), out_dir, from_encoding, dry_run, move, verbose)
 	elif (os.path.isfile(path)):
 		# Ignore ID3v1, no one cares anyway
 		try:
@@ -48,86 +87,41 @@ def convert(path, out_dir, from_encoding, to_encoding, dry_run, move, verbose):
 			tag_exists = False
 
 		if tag_exists:
-			songname = ''
-			artist = ''
-			all_correct_encoding = True
+			all_encoding_is_correct = True
 			for frame in id3.frames:
-				try:
-					print_verbose(verbose, "BEFORE: %s (%s) %s" % \
-						(frame.fid, frame.encoding, str(frame.strings)))
-					is_correct_encoding = True
-					if len(frame.strings) > 0:
-						newframestr = u''
-						# Check if correct encoding
-						if frame.encoding == 'utf_16':
-							is_correct_encoding = True
-						else:
-							for framestr in frame.strings:
-								try:
-									framestr.encode(frame.encoding)
-								except:
-									print "COULD NOT ENCODE "+frame.fid
-									is_correct_encoding = False
-									all_correct_encoding = False
-						# Create string with corrent encoding
-						for framestr in frame.strings:
-							if is_correct_encoding:
-								print_verbose(verbose, 'IF: '+framestr.encode(frame.encoding))
-								try:
-									newframestr += framestr
-								except Exception, e:
-									#noop = 0
-									print e
-							else:
-								try:
-									print_verbose(verbose, 'ELSE: '+framestr.decode(from_encoding))
-									newframestr += framestr.decode(from_encoding)
-								except Exception, e:
-									newframestr = u''
-									print e
-						if frame.fid == 'TIT2' or frame.fid == 'TT2': #TT2 is v2.2
-							songname = newframestr
-							print_verbose(verbose, 'SONGNAME: '+songname)
-						elif frame.fid == 'TPE1' or frame.fid == 'TP1':
-							artist = newframestr
-							print_verbose(verbose, 'ARTIST: '+artist)
-						# Replace string if it was previously bad
-						if not is_correct_encoding:
-							frame.set_text(newframestr, to_encoding)
-					print_verbose(verbose, (" AFTER: %s (%s) %s" % \
-						(frame.fid, frame.encoding, str(frame.strings))))
-				except Exception, e:
-					noop = 0
-					print_verbose(verbose, ("%s - unprintable" % frame.fid))
-					print_verbose(verbose, e)
-			if not all_correct_encoding:
-				# TODO fix the problem where an errant '\x00' screws everything up
-				if len(artist) == 0 or len(songname) == 0:
-					print "UNKNOWN ARTIST: %s OR SONGNAME: %s" % (artist, songname)#(map(hex, map(ord, artist)), map(hex, map(ord, songname)))
-				else:
-					filename = '%s - %s.mp3' % (artist, songname)
-					filename = filename.replace('\x00','').replace('/','-')
-					print filename
-					if not dry_run:
-						id3.commit_to_file(os.path.join(out_dir, filename))
-						if move:
-							try:
-								os.remove(path)
-							except Exception, e:
-								print("Could not remove "+path)
-			#id3.commit_to_file(id3.artist.decode('euc-kr')+' - '+id3.songname.decode('euc-kr')+'.mp3')
+				print_verbose(verbose, "BEFORE: %s (%s) %s" % \
+					(frame.fid, frame.encoding, str(frame.strings)))
 
-		# TODO: just change the filename encoding, but this is more difficult!
-		# Can't figure out how the filepath is encoded/decoded
-		# Instead, rename to "artist - songname"?
+				frame_encoding_is_correct = True
+				# Check if at least one incorrect encoding
+				for framestr in frame.strings:
+					if not is_encoding_correct(framestr, frame.encoding):
+						print "Found bad encoding "+path+", "+frame.fid
+						frame_encoding_is_correct = False
+						all_encoding_is_correct = False
+				# If at least one incorrect encoding, convert all to UTF-8
+				if not frame_encoding_is_correct:
+					frame.encoding = "utf_8"
+					for idx, framestr in enumerate(frame.strings):
+						try:
+							frame.strings[idx] = decode_string(framestr, from_encoding)
+							print_verbose(verbose, "reencoded string %s" % frame.strings[idx])
+						except Exception, e:
+							print "Could not convert "+path+", "+frame.fid+" using "+from_encoding
+							logging.exception("Stack Trace")
+							# TODO: stop looking at this file
+				if frame.fid == 'TPE1' or frame.fid == 'TP1':
+					artist = frame_strings_to_string(frame.strings)
+				elif frame.fid == 'TIT2' or frame.fid == 'TT2':
+					songname = frame_strings_to_string(frame.strings)
 
-		#print type(path)
-		#print map(ord, '1-01')
-		#print map(ord, '.mp3')
-		#print map(ord, path)[16:]
-		#print map(hex, map(ord, path))[16:]
-		#print path
-		#print path.decode('euc_kr')#.encode('utf-8')
+				print_verbose(verbose, (" AFTER: %s (%s) %s" % \
+					(frame.fid, frame.encoding, str(frame.strings))))
+
+			if not all_encoding_is_correct:
+				filepath = create_filepath(out_dir, artist, songname)
+				if not dry_run:
+					write_file(id3, filepath, move)
 	elif (os.path.exists(path)):
 		print 'Error, '+path+' exists but is neither a file or dir'
 	else:
